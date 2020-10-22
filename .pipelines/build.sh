2bdfbf536c214825ae0d9c9ed37a431439fbd0f6
heyaWorld <47616490+heyaWorld@users.noreply.github.com>
2020-10-21 19:00:38 -0700

    release pipeline and other files from main (#9)

---
+++ .pipelines/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJDIR="$(dirname "$DIR")"
+cd "$PROJDIR"
+
+if [ -d "$PROJDIR/build" ]; then
+  rm -rf $PROJDIR/build
+fi
+
+echo "Run makefile to generate packages for all the flavors"
+make package
